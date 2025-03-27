ab1dc172e5747686a4318bc116b85f23e82ffea3
RePlicantt <vladlen12121@gmail.com>
2025-03-27 19:09:27 +0200

    Initial commit

---
+++ src/main/java/com/replicant/jobee/JobeeApplication.java
@@ -0,0 +1,13 @@
+package com.replicant.jobee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/replicant/jobee/JobeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.replicant.jobee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
